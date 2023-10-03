d3ca018a46d17232c1faf34dc70194ea001c0289
Padam Damai <padamdamai888@gmail.com>
2023-10-03 16:37:47 +0545

    added contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,6 +4,37 @@ export default function Resume(){
 
 	return <>
 
-			<div>This is contact page</div>
+			<div className="container_main">
+
+				<div className="leftHandContainer">
+					<p className="helloFriend">Hello!! friend </p>
+					<p className="helpHow">Tell me how can we help <br/>you</p>
+
+					<div className="letCommunicate">Lets communicate with each other </div>
+					<div className="mail_content">
+						<p> Mail me at </p>
+						<div>padamdamai888@gmail.com </div>
+					</div>
+				</div>
+
+				<div className="rightHandContainer"> 
+					<form className="form">
+						<section className="sendMessage">Send me a message</section>
+						<div className="label_holder"> 
+							<label><input type="text"  className="inputType"  placeholder="Full Name" name="fullName"/> </label><br/>
+							<label><input type="email" className="inputType"  placeholder="E-Mail" name="e_mail" required/> </label><br/>
+							<label><input type="text"  className="inputType" placeholder="Subject" name="subject"/> </label><br/>	
+						</div>
+						<p className="formSmallElement"> How can we help you .</p>
+						<label for="textArea">
+							<textArea rows="4" cols="36" placeholder="leve us message"> 
+								
+							</textArea>
+						 </label><br/>
+
+						<input type="buttom" type="submit" className="btn" value="Submit" />
+					</form>
+				</div>
+			</div>
 		</>
 }
