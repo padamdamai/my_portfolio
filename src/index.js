190a74d477f96fc4c3468364e6c7a6bf4b510559
Padam Damai <padamdamai888@gmail.com>
2023-10-03 16:45:21 +0545

    added contact.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './resume.css';
 import './project.css';
+import './contact.css';
+
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
