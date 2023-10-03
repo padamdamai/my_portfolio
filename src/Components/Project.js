252ed663d4afc0bc021c86ccccc93eaac67ee757
Padam Damai <padamdamai888@gmail.com>
2023-10-03 16:41:26 +0545

    added object api

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,9 +1,40 @@
 import React from "react";
 
-export default function Resume(){
 
+export default function Project(props){
 	return <>
-
-			<h1>This is Project page</h1>
-		</>
+		
+		
+				<div className="card_holder">
+					<a href="" style={{textDecoration:'none',color:"black"}}>
+					<div className="card">
+						<div className="image_card"><img src={props.image} className="image"  alt="img"/> </div>	
+						<div className="image_title">{props.title} </div>	
+						<div className="project_description">{props.description}</div>			
+					</div>
+					</a>
+				</div>
+		
+			
+			</>
 }
+
+		/// <div className="title">
+	// 			My Project
+	// 		</div>
+
+	// 		<div className="description">
+	// 				Project helps us developing our skills  and develops more knowledge .As more project you do more experience you will gain
+	// 		</div>
+
+	// 		<div className="card_container">
+	// 			<div className="card_holder">
+	// 				<a href="" style={{textDecoration:'none',color:"black"}}>
+	// 				<div className="card">
+	// 					<div className="image_card"><img src={props.image} className="image"  alt="img"/> </div>	
+	// 					<div className="image_title">{props.title} </div>	
+	// 					<div className="project_description">{props.description}</div>			
+	// 				</div>
+	// 				</a>
+	// 			</div>
+	// 		</div>
