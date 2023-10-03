921f3c78b3f00ddf7a39f072a94cebd7cff9b595
Padam Damai <padamdamai888@gmail.com>
2023-10-03 16:40:31 +0545

    modified margin of top navbar and hover effect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,16 @@
 	import React from "react"
-  import { NavLink } from "react-router-dom";
+  import {Link } from "react-router-dom";
 
 
 export default function Navbar(){
 	return <div>
 	<div className="topnav">
-  <NavLink className="name_user" to="/">Padam Damai</NavLink>
+  <Link className="name_user" to="/">Padam Damai</Link>
   <div className="topnav-right">
 
-    <NavLink to="resume">Resume</NavLink>
-    <NavLink to="projects">Project</NavLink>
-    <NavLink to="contact">Contact</NavLink>
+    <Link to="resume">Resume</Link>
+    <Link to="projects">Project</Link>
+    <Link to="contact">Contact</Link>
   </div>
 </div>
 
