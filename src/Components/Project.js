4114de844910855f393b789e6c27f22bbbc52e77
Padam Damai <padamdamai888@gmail.com>
2023-10-03 16:49:34 +0545

    deleted comment line

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -19,22 +19,3 @@ export default function Project(props){
 			</>
 }
 
-		/// <div className="title">
-	// 			My Project
-	// 		</div>
-
-	// 		<div className="description">
-	// 				Project helps us developing our skills  and develops more knowledge .As more project you do more experience you will gain
-	// 		</div>
-
-	// 		<div className="card_container">
-	// 			<div className="card_holder">
-	// 				<a href="" style={{textDecoration:'none',color:"black"}}>
-	// 				<div className="card">
-	// 					<div className="image_card"><img src={props.image} className="image"  alt="img"/> </div>	
-	// 					<div className="image_title">{props.title} </div>	
-	// 					<div className="project_description">{props.description}</div>			
-	// 				</div>
-	// 				</a>
-	// 			</div>
-	// 		</div>
