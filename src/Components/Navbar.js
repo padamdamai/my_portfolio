66c0fe3d68c53e35c267cb950c1822b105784b8a
Padam Damai <padamdamai888@gmail.com>
2023-10-09 16:47:29 +0545

    added outside click react hook

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,13 +4,30 @@
 
 
 export default function Navbar(){
-
+let menuRef = React.useRef();
  const[toggleMenu,setToggleMenu] = React.useState(false);
+
+React.useEffect(()=>{
+const handelor = (event) =>{
+  if(!menuRef.current.contains(event.target)){
+    setToggleMenu(false)
+      }
+      }
+
+      document.addEventListener("mousedown", handelor);
+
+      return() => {
+        document.removeEventListener("mousedown", handelor);
+      }
+});
+
+
+
 
 	return <div>
 	<div className="topnav">
   <Link className="name_user" to="/">Padam Damai</Link>
-  <div className={toggleMenu ? "topnav-right":"hide_menu_bar"} style={{float:"right"}}>
+  <div ref={menuRef} className={toggleMenu ? "topnav-right":"hide_menu_bar"} style={{float:"right"}}>
 
     <Link to="resume">Resume</Link>
     <Link to="projects">Project</Link>
@@ -26,4 +43,3 @@ export default function Navbar(){
 	</div>
 }
 
-{/* setToggleMenu(previousValue => (!previousValue)) */}
